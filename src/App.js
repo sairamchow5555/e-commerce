185ac0b9e8efaef6f1cef69a9146327a821c0b8c
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-20 16:57:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found.!</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
