d31b80ecc39d0741b73c9e7af0eaa66242b298dd
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-20 16:58:01 +0530

    store data

---
+++ src/redux/Store.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import reducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(
+    reducer,
+    {},
+    composeWithDevTools()
+)
+
+export default store;
