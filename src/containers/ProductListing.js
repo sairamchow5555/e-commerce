3824745fc116058578f3af59a2e4f43581078d68
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-21 12:22:16 +0530

    last commit

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -9,15 +9,15 @@ const ProductListing = () => {
     const dispatch = useDispatch();
 
     useEffect(()=>{
-    const fetchProducts = async () => {
-        const response = await axios
-        .get("https://fakestoreapi.com/products")
-        .catch((err) => {
-            console.log("Err", err);
-        });
-        dispatch(setProducts(response.data));
-    };
-        fetchProducts();
+        const fetchProducts = async () => {
+            const response = await axios
+            .get("https://fakestoreapi.com/products")
+            .catch((err) => {
+                console.log("Err", err);
+            });
+            dispatch(setProducts(response.data));
+        };
+            fetchProducts();
     }, [dispatch]);
 
     console.log("Products is:",products);
